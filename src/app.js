eefe4e54444ea5130f5e490a66d9773eeb69af84
joaquin_eas@hotmail.com <joaquin_eas@hotmail.com>
2021-01-20 15:07:45 -0300

    ordene las carpetas y todas las rutas del sprint 2

--- src/app.js
+++ src/app.js
@@ -20,3 +20,11 @@ app.get('/login', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.render(__dirname + '/views/register.ejs');
 });
+
+app.get('/carrito', (req,res)=>{
+    res.render(__dirname + '/views/carritoDeCompras.ejs');
+});
+
+app.get('/provincia', (req,res)=>{
+    res.render(__dirname + '/views/provincia1.ejs');
+});
