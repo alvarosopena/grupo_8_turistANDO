30b7d9029dce8f968094d8145a3e12b167faa28b
joaquin_eas@hotmail.com <joaquin_eas@hotmail.com>
2021-01-20 12:17:52 -0300

    Ordené las carpetas

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+
+
+app.listen(process.env.PORT || 3001, ()=>{
+    console.log('Servidor funcionando');
+});
+app.set ("view engine" , "ejs")
+
+app.get('/', (req,res)=>{
+    res.render(__dirname + '/views/home.ejs');
+});
+
+app.get('/login', (req,res)=>{
+    res.render(__dirname + '/views/login.ejs');
+});
+
+app.get('/register', (req,res)=>{
+    res.render(__dirname + '/views/register.ejs');
+});
