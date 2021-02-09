2eb509355d1be24277dbcd2dea88a1ccca527f6b
florencia-belmana <florbel_03@hotmai.com>
2021-02-08 21:30:16 -0300

    rutas y modificacion de provincia CRUD

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -6,4 +6,19 @@ router.get( '/provincia' , controller.provincia ) ;
 router.get( '/carrito' , controller.carrito ) ;
 
 
+//rutas estimativas//
+
+router.get('/', controller.provincia);
+router.get('/create', controller.create);
+router.get('/:id', controller.show); //ver//
+router.post('/', controller.store);//ver//
+router.get('/:id/edit', controller.edit);
+router.put('/:id', controller.update);
+router.delete('/:id', controller.destroy);
+
+
+
+
+
+
 module.exports = router;
